723dc682e4a4295d377c77b22efcccfc547ba236
Vishal Shukla <168364704+vishshukla1234@users.noreply.github.com>
2026-03-10 22:13:52 +0530

    Update TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,3 +1,4 @@
+// -----------------------------------------NAIVE APPROACH-------------------------------------------
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
 
@@ -12,5 +13,64 @@ public class TwoSum {
         }
 
         return new int[] {};
+    }
+}
+// ---------------------------BETTER APPROACH---------------------------------------------------------
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int n = nums.length;
+
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+
+            int sum = arr[left][0] + arr[right][0];
+
+            if(sum == target) {
+                return new int[]{arr[left][1], arr[right][1]};
+            }
+            else if(sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return new int[]{};
+    }
+}
+// ---------------------------OPTIMAL APPROACH---------------------------------------------------------
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
     }
 }
