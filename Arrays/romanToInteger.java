81c923181ca6ff2a9e1ae8357080b6d063621c38
Vishal <shuklav8552@gmail.com>
2026-07-21 22:03:54 +0530

    Add Roamn To Integer Solution

---
+++ Arrays/romanToInteger.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class romanToInteger {
+    public static int helper(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(i+1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
+                ans -= map.get(s.charAt(i));
+            } else{
+                ans += map.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "MXI";
+        System.out.println(helper(s));
+    }
+}
